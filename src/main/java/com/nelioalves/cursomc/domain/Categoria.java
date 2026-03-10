f45820ec7014918431788d1dc6440468b4c1d175
Welinton Azevedo <welintonz226@gmail.com>
2026-03-10 19:56:14 -0300

    Banco de dados H2 e criação automática da base de
    dados

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -1,12 +1,23 @@
-package com.nelioalves.cursomc.cursomc.domain;
+package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	
 	public Categoria() {
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,4 +1,4 @@
-package com.nelioalves.cursomc.cursomc.resources;
+package com.nelioalves.cursomc.resources;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nelioalves.cursomc.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
